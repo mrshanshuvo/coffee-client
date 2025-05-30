fb7246b8b749bbee06da474bda8594287c8f9947
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-05-30 21:02:41 +0600

    initial commit

--- src/components/CoffeeDetails.jsx
+++ src/components/CoffeeDetails.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const CoffeeDetails = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default CoffeeDetails;
